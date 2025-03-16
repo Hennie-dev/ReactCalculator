1a8359e6ba82f51469fdc9cddfdb864f61c6f74b
Hennie <henniehjvanheerden@gmail.com>
2025-03-16 21:46:03 +0200

    Added eval functionality for calculation logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,44 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [result, setResult] = useState('');
+
+  const clickHandle = (Event) => {
+    setResult(result.concat(Event.target.value));
+  }
+
+  const clear = () => {
+    setResult('');
+  }
+
+  const calculateResult = () => {
+    setResult(eval(result));
+  }
+
   return (
     <>
       <div className='Calculator'>
-        <input type='text' placeholder='0' id='result'/>
-        <input type='button' className='button' value='9' />
-        <input type='button' className='button' value='8' />
-        <input type='button' className='button' value='7' />
-        <input type='button' className='button' value='6' />
-        <input type='button' className='button' value='5' />
-        <input type='button' className='button' value='4' />
-        <input type='button' className='button' value='3' />
-        <input type='button' className='button' value='2' />
-        <input type='button' className='button' value='1' />
+        <input type='text' placeholder='0' id='result' value={result}/>
+        <input type='button' className='button' value='9' onClick={clickHandle}/>
+        <input type='button' className='button' value='8' onClick={clickHandle}/>
+        <input type='button' className='button' value='7' onClick={clickHandle}/>
+        <input type='button' className='button' value='6' onClick={clickHandle}/>
+        <input type='button' className='button' value='5' onClick={clickHandle}/>
+        <input type='button' className='button' value='4' onClick={clickHandle}/>
+        <input type='button' className='button' value='3' onClick={clickHandle}/>
+        <input type='button' className='button' value='2' onClick={clickHandle}/>
+        <input type='button' className='button' value='1' onClick={clickHandle}/>
 
-        <input type='button' className='button' value='0' />
-        <input type='button' className='button' value='+' />
-        <input type='button' className='button' value='-' />
-        <input type='button' className='button' value='/' />
-        <input type='button' className='button' value='*' />
-        <input type='button' className='button' value='%' />
+        <input type='button' className='button' value='0' onClick={clickHandle}/>
+        <input type='button' className='button' value='+' onClick={clickHandle}/>
+        <input type='button' className='button' value='-' onClick={clickHandle}/>
+        <input type='button' className='button' value='/' onClick={clickHandle}/>
+        <input type='button' className='button' value='*' onClick={clickHandle}/>
+        <input type='button' className='button' value='%' onClick={clickHandle}/>
 
-        <input type='button' className='button' value='=' />
-        <input type='button' className='button end' value='Clear' />
-        <input type='button' className='button end' value='=' />
+        <input type='button' className='button' value='.' onClick={clickHandle}/>
+        <input type='button' className='button end' value='Clear' onClick={clear}/>
+        <input type='button' className='button end' value='=' onClick={calculateResult}/>
       </div>  
     </>
   )
